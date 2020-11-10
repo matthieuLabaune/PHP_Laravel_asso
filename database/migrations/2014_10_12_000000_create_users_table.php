f1434ee3dfc41076578201c4f59f23ac1ceb3060
Matthieu <matthieu.labaune@le-campus-numerique.fr>
2020-11-10 13:47:59 +0100

    [ADD] · UserController with resources

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('first-name');
+            $table->string('gender');
+            $table->date('birthdate');
+            $table->string('phone');
+            $table->string('city');
+            $table->integer('postal_code');
+            $table->string('address');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
